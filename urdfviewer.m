6965615148cfa521b44d84e22d61a41dfee7f3e3
Great GIlbert Soco <gilsocojp@gmail.com>
2026-06-10 19:26:54 +0900

    Added urdfviewer

---
+++ urdfviewer.m
@@ -0,0 +1,5 @@
+cd('C:\Users\Lanie\Documents\Ri-One\Projects\Open Manipulator X6\URDF\Complete Arm Assembly\urdf')
+robot = importrobot('Complete Arm Assembly.urdf');
+figure
+show(robot,'Frames','on');
+axis equal

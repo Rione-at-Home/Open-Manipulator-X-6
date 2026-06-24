6539076d0082ce75d88b76346e94cb347d54822f
Great GIlbert Soco <gilsocojp@gmail.com>
2026-06-24 16:10:48 +0900

    Updated urdfviewer to show the paperbag models

--- urdfviewer.m
+++ urdfviewer.m
@@ -1,5 +1,19 @@
-cd('C:\Users\Lanie\Documents\Ri-One\Projects\Open Manipulator X6\URDF\Complete Arm Assembly_v3\Complete Arm Assembly\urdf')
-robot = importrobot('Complete Arm Assembly.urdf');
+%
+%% urdfviewer.m
+%
+% The purpose of this code is to provide a visualization step before loading the model
+% in Gazebo. This is just to ensure that the urdf models generated in
+% SW2URDF plugin are generated properly.
+%
+
+% Open Manipulator X6 arm
+% cd('C:\Users\Lanie\Documents\Ri-One\Projects\Open Manipulator X6\complete_arm_assembly\urdf')
+% model = importrobot('complete_arm_assembly.urdf');
+
+
+% Paperbag Model
+cd('C:\Users\Lanie\Documents\Ri-One\Projects\Open Manipulator X6\paperbag\urdf')
+model = importrobot('paperbag.urdf');
 figure
-show(robot,'Frames','on');
+show(model,'Frames','on');
 axis equal

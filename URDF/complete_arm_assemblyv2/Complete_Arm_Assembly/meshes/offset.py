b67e4a0b3b2632ab07de37f6f6b55860610295f0
Gil-gil-glitch <gilsocojp@gmail.com>
2026-07-27 17:56:20 +0900

    Removed all simulation files

--- URDF/complete_arm_assemblyv2/Complete_Arm_Assembly/meshes/offset.py
+++
@@ -1,9 +0,0 @@
-import trimesh
-
-link5 = trimesh.load("link_5.STL")
-right_finger = trimesh.load("right_finger_link.STL")
-
-# Both meshes are in the same global frame originally,
-# so the offset between their centroids/origins is the relative transform
-offset = right_finger.centroid - link5.centroid  # or use bounding_box.centroid
-print(offset)
--- urdfviewer.m
+++
@@ -1,5 +0,0 @@
-cd('C:\Users\Lanie\Documents\Ri-One\Projects\Open Manipulator X6\URDF\Complete Arm Assembly_v3\Complete Arm Assembly\urdf')
-robot = importrobot('Complete Arm Assembly.urdf');
-figure
-show(robot,'Frames','on');
-axis equal

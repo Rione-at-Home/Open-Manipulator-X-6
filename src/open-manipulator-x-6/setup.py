5e2513ad60e5b11e5fadebbb0a62510fe01cd628
Gil-gil-glitch <gilsocojp@gmail.com>
2026-07-27 18:16:42 +0900

    Updated the package.xml and the setup.py to run the package

--- src/open-manipulator-x-6/setup.py
+++ src/open-manipulator-x-6/setup.py
@@ -1,30 +1,28 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'Open-Manipulator-X-6'
+package_name = 'open_manipulator_x6'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
-    zip_safe=True,
+    zip_safe=true,
     maintainer='ri-one',
-    maintainer_email='gilsocojp@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer_email='dev@todo.todo',
+    description='ROS 2 driver for 6-DOF OpenManipulator-X arm.',
+    license='MIT',
     entry_points={
         'console_scripts': [
-            'my_node = Open-Manipulator-X-6.my_node:main'
+            'arm_driver = open_manipulator_x6.arm_driver:main',
         ],
     },
 )
